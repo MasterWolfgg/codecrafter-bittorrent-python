60ced28f53251b2a04551f2f2612c833c554c5a7
Sarbobhowma <sarbobhowmasen12345@gmail.com>
2024-08-25 22:08:14 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def main():
         print(f'Tracker URL: {info_dict[b"announce"].decode()}')
         print(f'Length: {info_dict[b"info"][b"length"]}')
         print(f"Info Hash: {info_hash.hexdigest()}")
-        print(f'Piece Length: {info_dict["info"]["piece length"]}')
+        print(f'Piece Length: {info_dict[b"info"][b"piece length"]}')
         print(f"Piece Hashes: ")
         for i in range(0, len(info_dict[b"info"][b"pieces"]), 20):
             print(info_dict[b"info"][b"pieces"][i : i + 20].hex())
