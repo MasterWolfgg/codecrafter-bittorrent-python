6a16d20c8afb881dbfe866656820dbc5beb784fe
Sarbobhowma <sarbobhowmasen12345@gmail.com>
2024-08-25 23:59:15 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -120,7 +120,7 @@ def main():
         with open(file_name, "rb") as f:
             bencoded_value=f.read()
         parsed = decode_bencode(bencoded_value)
-        info=parsed.get["info"]
+        info=parsed.get("info")
         bencoded_info=bencodepy.encode(info)
         info_hash = hashlib.sha1(bencoded_info).digest()
             
